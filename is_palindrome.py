45b7cff52a3de815b1164003de779af6cae531f9
Aman Babu Hemant <134631394+amanbabuhemant@users.noreply.github.com>
2023-06-04 01:09:43 +0530

    Added is_palindrome.py

---
+++ is_palindrome.py
@@ -0,0 +1,28 @@
+def is_palindrome(n: int):
+    r = 0
+    t = n
+    while n > 0:
+        r = r * 10 + n % 10
+        n //= 10
+    return r == t
+
+def is_palindrome(s: str):
+    return s == s[::-1]
+
+def is_palindrome(s: list):
+    s = list(s)
+    return s == s[::-1]
+
+def is_palindrome(s):
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+def is_palindrome(s):
+    s = str(s)
+    return s == s[::-1]
