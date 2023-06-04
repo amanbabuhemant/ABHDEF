e5c02346b47d1c3fdb032489cb7f4d811a6f609e
Aman Babu Hemant <134631394+amanbabuhemant@users.noreply.github.com>
2023-06-04 15:09:51 +0530

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,6 +1,6 @@
 #Check the given string, number or list is palindrome or not.
 
-def is_palindrome(n: int):
+def is_palindrome(n: int) -> bool:
     r = 0
     t = n
     while n > 0:
@@ -8,14 +8,14 @@ def is_palindrome(n: int):
         n //= 10
     return r == t
 
-def is_palindrome(s: str):
+def is_palindrome(s: str) -> bool:
     return s == s[::-1]
 
-def is_palindrome(s: list):
+def is_palindrome(s: list) -> bool:
     s = list(s)
     return s == s[::-1]
 
-def is_palindrome(s):
+def is_palindrome(s) -> bool:
     left = 0
     right = len(s) - 1
     while left < right:
@@ -25,6 +25,6 @@ def is_palindrome(s):
         right -= 1
     return True
 
-def is_palindrome(s):
+def is_palindrome(s) -> bool:
     s = str(s)
     return s == s[::-1]
