a3df319ca5c0f6b71efa8411cd99dd451a35a8cb
Aman Babu Hemant <134631394+amanbabuhemant@users.noreply.github.com>
2023-06-18 01:15:04 +0530

    Publish factorial.py

---
+++ factorial.py
@@ -0,0 +1,13 @@
+#Calculate the factorial of given number
+
+def factorial(n: int) -> int:
+    if n == 0:
+        return 1
+    return factorial(n-1) * n
+
+def dfactorial(n: int) -> int:
+    f = 1
+    while n:
+        f *= n
+        n -= 1
+    return f
