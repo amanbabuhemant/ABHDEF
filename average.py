183c2c9d9df7558e70992f5bae6c3bfb5334c975
Aman Babu Hemant <amanbabuhemant@gmail.com>
2023-07-07 23:42:48 +0530

    average function added

---
+++ average.py
@@ -0,0 +1,21 @@
+def average(*nums):
+    s = 0
+    c = 0
+    for n in nums:
+        s += n
+        c += 1
+    return s / c
+
+def average(*nums):
+    return sum(nums)/len(nums)
+
+def average(nums):
+    return sum(nums)/len(nums)
+
+def average(nums):
+    s = 0
+    c = 0
+    for n in nums:
+        s += n
+        c += 1
+    return s / c
