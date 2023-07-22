0dea61317472cca71693573604f8cc5848aed4fb
Aman Babu Hemant <amanbabuhemant@gmail.com>
2023-07-23 01:36:39 +0530

    is_happy added

---
+++ is_happy.py
@@ -0,0 +1,23 @@
+def is_happy(n: int):
+    n = str(n)
+    while len(n) != 1:
+        s = 0
+        for d in n:
+            s += int(d)**2
+        n = str(s)
+    return n in {"1", "7"}
+
+def is_happy(n: int):
+    while n > 9:
+        s = 0
+        t = n
+        while t:
+            s += (t%10) **2
+            t //= 10
+        n = s
+    return n == 1 or n == 7
+
+def is_happy(n: int):
+    while n > 9:
+        n = sum(int(d)**2 for d in str(n))
+    return n == 1 or n == 7
