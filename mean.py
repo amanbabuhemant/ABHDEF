b971706f66acf4325c1667f6b2ebabbbe895718a
Aman Babu Hemant <amanbabuhemant@gmail.com>
2023-09-25 00:22:21 +0530

    mean added

---
+++ mean.py
@@ -0,0 +1,21 @@
+def mean(*nums):
+    s = 0
+    c = 0
+    for n in nums:
+        s += n
+        c += 1
+    return s / c
+
+def mean(nums):
+    s = 0
+    c = 0
+    for n in nums:
+        s += n
+        c += 1
+    return s / c
+
+def mean(*nums):
+    return sum(nums)/len(nums)
+
+def mean(nums):
+    return sum(nums)/len(nums)
